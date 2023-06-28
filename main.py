74e07459a3d8ed38a32dd298c01bfe35f1c72504
HitenBhadra <67404448+HitenBhadra@users.noreply.github.com>
2023-06-28 23:53:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import requests ,json
 import streamlit as st
 
 response=requests.get("https://api.quotable.io/quotes/random")
-qd=response.json()
-q=qd[0]['content']
 
 #ui
 st.set_page_config(
         page_title="Quote of the day",
 )
+
+st.title('Quote of the day')
 c=1
 while c==1:
-  c=0
-  st.title('Quote of the day')
-  st.write(f'{q} \n - Gopal')
-  if st.button('New quote'):
-    c=1
+    c=0
+    qd=response.json()
+    q=qd[0]['content']
+    st.write(f'{q} \n - Gopal')
+    l=st.button('New quote')
+    l=False
+    if l:
+        c=1
+  
       
 
 
