56c4569eed8917d3a98dc8771e8ae6f34fab1e82
HitenBhadra <67404448+HitenBhadra@users.noreply.github.com>
2023-06-29 21:24:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests ,json 
-import threading as thr
 import streamlit as st
 
 qapi=requests.get("https://api.quotable.io/quotes/random")
@@ -23,26 +22,24 @@ def quote():
         if l:
             c=1
 
-def dictionary():
-    l=1
-    while l==1:
-        l=0
-        word=st.text_input('Enter word to search','query',key='placeholder')
-        dapi=requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
-        dd=dapi.json()
-        for d in dd:
-        
-            st.write(f'{dd[d]}')
-        l=st.button('Reset')
-        l=False
-        if l:
-            l=1
 
-st.subheader('Dictionary')
-t1=thr.Thread(target=quote)
-t2=thr.Thread(target=dictionary)
-t1.start()
-t2.start()
+        f=1
+        st.subheader('Dictionary')
+        while f==1:
+            f=0
+            word=st.text_input('Enter word to search','query',key='placeholder')
+           dapi=requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+            dd=dapi.json()
+            for d in dd:
+        
+                st.write(f'{dd[d]}')
+            f=st.button('Reset')
+            f=False
+            if l:
+                f=1
+
+
+quote()
 
 
 
